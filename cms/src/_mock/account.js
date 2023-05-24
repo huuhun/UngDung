2516fabdb2f0a7be2179910d972748dde5d16fd1
huuhun <letranhuuhung@gmail.com>
2023-05-24 07:53:37 +0700

    init project

---
+++ cms/src/_mock/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'admin',
+  email: 'letranhuuhung@gmail.com',
+  photoURL: '/assets/images/avatars/avatar_25.jpg',
+};
+
+export default account;
